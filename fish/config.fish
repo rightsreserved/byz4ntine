81b9d69f3554f39926de6be43daeba27bc76a447
rightsreserved <66631259+rightsreserved@users.noreply.github.com>
2024-02-11 21:36:26 +0000

    Update config.fish
    clean up

--- fish/config.fish
+++ fish/config.fish
@@ -15,35 +15,20 @@ abbr upg "sudo pacman -Syu"
 abbr i "sudo pacman -Sy && sudo pacman -S"
 abbr sc "trizen -Ss"
 abbr key "sudo pacman-key --refresh-keys"
-abbr mca "java --module-path Documents/javaJFX/javafx-sdk-16/lib/ --add-modules ALL-MODULE-PATH -jar Downloads/mcaselector-1.16.3.jar
-"
 
 # config
 abbr config "micro ~/.config/i3/config"
-abbr swayconf "micro ~/.config/sway/config"
-abbr piconf "micro ~/.config/picom/picom.conf"
-abbr polyconf "micro ~/.config/polybar/config"
+abbr piconf "micro ~/.config/picom.conf"
 abbr fishconf "micro ~/.config/fish/config.fish"
 abbr termconf "micro ~/.config/termite/config"
-abbr miniconf "micro ~/.config/miniterm/miniterm.conf"
-abbr alacconf "micro ~/.config/alacritty/alacritty.yml"
+abbr alacconf "micro ~/.config/alacritty/alacritty.toml"
 abbr dunstconf "micro ~/.config/dunst/dunstrc"
-abbr promptconf "micro ~/.config/starship.toml"
 
 # clear screen
 abbr a "clear && pfetch"
-abbr s "clear && neofetch | lolcat"
-abbr d "clear && nekofetch"
 abbr f "clear && figlet faith | rainbow"
-abbr c "clear && neofetch | pridecat --trans"
-abbr x "clear && fet.sh"
-abbr z "clear && colorscript exec colorbars"
-abbr v "clear && neofetch | pridecat --trans --lesbian"
-abbr b "clear && colorscript exec colortest"
+abbr x "clear && neofetch"
+abbr a "clear && fet.sh"
 
 # start up theming
-neofetch | pridecat --trans
-
-# prompt
-
-# starship init fish | source
+pfetch
